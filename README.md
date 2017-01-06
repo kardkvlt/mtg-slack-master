5ceade4e67ab3f12f7f92007893eb7872bd19bf9
kardkvlt <nickkorzun@gmail.com>
2017-01-06 01:07:12 -0500

    mtg
    MTG bot

---
+++ README.md
@@ -0,0 +1,17 @@
+# mtg-slack
+### Slack slash command that retrieves Magic: The Gathering card information
+![example image](https://raw.githubusercontent.com/jeffcampbell/mtg-slack/master/readme/screenshot.png)
+
+### What you will need
+* A [Heroku](http://www.heroku.com) account
+* A [slash command outgoing webhook token](https://api.slack.com/slash-commands) for your Slack team
+
+### Setup
+* Clone this repo locally
+* Create a new Heroku app and initialize the repo
+* Push the repo to Heroku
+* Navigate to the settings page of the Heroku app and add the following config variables:
+  * ```OUTGOING_WEBHOOK_TOKEN``` The token for your slash command integration in Slack
+* Navigate to the integrations page for your Slack team. Create an slash command, use the URL for your heroku app, and copy the webhook token to your ```OUTGOING_WEBHOOK_TOKEN``` config variable. Give the command a name and a cool icon. That's it!
+
+Thanks to https://deckbrew.com/api/ for their awesome API
---
+++ app.rb
@@ -0,0 +1,88 @@
+# encoding: utf-8
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
+configure do
+  # Load .env vars
+  Dotenv.load
+  # Disable output buffering
+  $stdout.sync = true
+end
+
+get "/" do
+  ""
+end
+
+post "/" do
+  response['content-type'] = 'application/json'
+  response = ""
+begin
+  puts "[LOG] #{params}"
+  params[:text] = params[:text]
+  unless params[:token] != ENV["OUTGOING_WEBHOOK_TOKEN"]
+    response = { text: generate_text }
+    response[:response_type] = "in_channel"
+    response[:attachments] = [ generate_attachment ]
+    response = response.to_json
+  end
+end
+  status 200
+  body response
+end
+
+def generate_request
+  @user_query = params[:text].gsub(/\s/,'+')
+
+  if @user_query.length == 0
+    uri = "https://api.deckbrew.com/mtg/colors"
+  else
+    uri = "https://api.deckbrew.com/mtg/cards?name=#{@user_query}"
+  end
+
+  request = HTTParty.get(uri)
+  puts "[LOG] #{request.body}"
+  result = JSON.parse(request.body)
+end
+
+def generate_text
+  if generate_request[0].nil?
+    response = "No matching card found."
+  else
+    @cardname = generate_request[0]["name"]
+    response = "#{@cardname}"
+  end
+  response
+end
+
+def generate_attachment
+  replacements = [ ["{W}", ":white_circle:"], ["{U}", ":large_blue_circle:"],["{B}", ":black_circle:"],["{R}", ":red_circle:"],["{G}", ":tennis:"],["{", ""],["}", ""] ]
+  if generate_request[0].nil?
+    response = ""
+  else
+  @cardtext = generate_request[0]["text"]
+  @imageurl = generate_request[0]["editions"][0]["image_url"]
+  @types = generate_request[0]["types"][0]
+  @cost = generate_request[0]["cost"]
+
+  replacements.each {|replacement| @cost.gsub!(replacement[0], replacement[1])}
+  replacements.each {|replacement| @cardtext.gsub!(replacement[0], replacement[1])}
+
+  response = {
+            text: "#{@cardtext}",
+            fields: [
+                {
+                    "title": "Types",
+                    "value": "#{@types}",
+                    "short": true
+                },
+                {
+                    "title": "Cost",
+                    "value": "#{@cost}",
+                    "short": true
+                },
+            ],
+            image_url: "#{@imageurl}" }
+  end
+response
+end
